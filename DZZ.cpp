605a618c63affc8c67d631df1e23938504a08db2
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-12-08 13:04:09 +0200

    DZZ

---
+++ DZZ.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Date
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct Phone
+{
+	string material;
+	string name;
+	string color;
+	Date created;
+	double weight;
+	double height;
+};
+
+struct Laptop
+{
+	string material;
+	string name;
+	string color;
+	Date created;
+	double weight;
+	double height;
+};
+
+struct IceCream
+{
+	string material;
+	string flavor;
+	string color;
+	string height;
+	string weight;
+	Date created;
+	bool itIsYummy;
+};
+
+struct Flat
+{
+	string ownerName;
+	string country;
+	string city;
+	string street;
+	int floor;
+	int number;
+	int rooms;
+	Date leasing;
+};
+
+struct Pen
+{
+	string name;
+	string color;
+	double length;
+	double weight;
+	bool itIsHelium;
+};
+
+int main()
+{
+}
